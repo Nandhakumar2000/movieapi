16f08b86d232ee65ab3ac27624cb495bf3523558
Nandhakumar2000 <38251577+Nandhakumar2000@users.noreply.github.com>
2020-05-27 10:38:58 +0530

    Add files via upload

---
+++ precache-manifest.8f6fc94d5c4f018f5c204ee67b81e838.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "487ec52782837fe01907b7bec4c872e8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1ec8d943f919b15aafb5",
+    "url": "./static/css/main.ed6b7c6b.chunk.css"
+  },
+  {
+    "revision": "85f9608659d71d723a7f",
+    "url": "./static/js/2.d65fda28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d65fda28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ec8d943f919b15aafb5",
+    "url": "./static/js/main.73630426.chunk.js"
+  },
+  {
+    "revision": "c0e7cf32c66b81e4fd3f",
+    "url": "./static/js/runtime-main.18c1d433.js"
+  }
+]);
